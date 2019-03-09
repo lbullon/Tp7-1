0808c33dd4307f9f24997dcc0f859bc381a5fb5c
CristianBY <cristianyelmo@gmail.com>
2019-03-09 22:14:16 +0100

    clase Producto e implementacion de DAOProducto

--- Producto.java
+++ Producto.java
@@ -1,41 +1,70 @@
-class Producto {
+public class Producto {
 
-    private int peso;
+    private Integer sku;
     private String name;
-    private double precio;
-    public Producto() {
-         peso = 0;
+    private Double precio;
+    private Double cantidad;
 
+    public Producto() {
+         sku = 0;
          name = "";
-         precio = 0;
-         
+         precio = 0.0;
+         cantidad = 0.0;      
     }
-    public Producto( int peso, String name, double precio) {
-        this.peso = peso;
+    public Producto( int sku, String name, double precio, double cantidad) {
+        this.sku = sku;
         this.name = name;
         this.precio = precio;
-   
-      
-    }
-    public void setPeso(int p) {
-      peso = p;
-    }
-    public int getPeso() {
-      return peso;
+        this.cantidad = cantidad; 
     }
 
-    public void setName(String n) {
-      name = n;
+    public void setSku(int sku) {
+      this.sku = sku;
+    }
+    public int getSku() {
+      return sku;
+    }
+
+    public void setName(String name) {
+      this.name = name;
     }
     public String getName() {
       return name;
     }
-    public void setPrecio(double pr) {
-      precio = pr;
+
+    public void setPrecio(double precio) {
+      this.precio = precio;
     }
     public double getPrecio() {
       return precio;
     }
 
+    public void setCantidad(double cantidad) {
+      this.cantidad = cantidad;
+    }
+    public double getCantidad() {
+      return cantidad;
+    }
+
+    @ Override
+    public String toString() {
+      return name+":"+sku+":"+precio+":"+cantidad;
+    }
+
+    @ Override
+    public boolean equals(Object o) {
+      if (o == null) return false;
+      if (o == this) return true;
+      if (!(o instanceof Producto)){
+        return false;
+      }
+      Producto producto = (Producto) o;
+      if(sku.equals(producto.getSku()) && name.equals(producto.getName()) && precio.equals(producto.getPrecio()) && cantidad.equals(producto.getCantidad())) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
 
 }
